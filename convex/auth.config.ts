fdc0d527eda469c23736d6c718522694236ba31c
Vishnu <vbalakrishnan@seek.com.au>
2024-02-26 14:39:13 +1100

    project structure,convex,clerk Ready

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+        {
+            domain: "https://exact-seahorse-69.clerk.accounts.dev",
+            applicationID: "convex",
+        },
+    ]
+};
---
+++ middleware.ts
@@ -0,0 +1,3 @@
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware({});
